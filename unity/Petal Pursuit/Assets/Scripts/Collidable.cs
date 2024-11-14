77392271806b7a4ec23f9c21857b878f3775b267
samuelsarzaba <samuelsarzaba@gmail.com>
2024-11-14 17:30:34 -0500

    Fixed error that was happening

--- unity/Petal Pursuit/Assets/Scripts/Collidable.cs
+++ unity/Petal Pursuit/Assets/Scripts/Collidable.cs
@@ -4,27 +4,31 @@ using UnityEngine;
 
 public class Collidable : MonoBehaviour
 {
-    private Collider2D collider;
-    [SerializeField]
-    private ContactFilter2D filter;
-    private List<Collider2D> collidedObjects = new List<Collider2D>(1); // just stores player
+  private Collider2D myCollider;
+  [SerializeField]
+  private ContactFilter2D filter;
+  private List<Collider2D> collidedObjects = new List<Collider2D>(1); // just stores player
 
 
-    protected virtual void Start() {
-        collider = GetComponent<Collider2D>();
+  protected virtual void Start()
+  {
+    myCollider = GetComponent<Collider2D>();
 
+  }
+
+  protected virtual void Update()
+  {
+    myCollider.OverlapCollider(filter, collidedObjects);
+    foreach (var o in collidedObjects)
+    {
+      //  Debug.Log("Collided with " + o.name);
+      OnCollided(o.gameObject);
     }
+  }
 
-    protected virtual void Update() {
-        collider.OverlapCollider(filter, collidedObjects);
-        foreach(var o in collidedObjects) {
-          //  Debug.Log("Collided with " + o.name);
-            OnCollided(o.gameObject);
-        }
-    }
-
-    protected virtual void OnCollided(GameObject collidedObject) {
-      //  Debug.Log("Collided with " + collidedObject.name);
-    }
+  protected virtual void OnCollided(GameObject collidedObject)
+  {
+    //  Debug.Log("Collided with " + collidedObject.name);
+  }
 }
 
