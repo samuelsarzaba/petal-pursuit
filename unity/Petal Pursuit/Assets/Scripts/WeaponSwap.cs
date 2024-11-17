42504ffa294a469d798f3522c4a001176ab8b5ca
James West <westjm7@gmail.com>
2024-11-17 18:16:22 -0500

    Added scenes and slight UI change

---
+++ unity/Petal Pursuit/Assets/Scripts/WeaponSwap.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwap : PlayerMovement
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // on click, swap array to player holding other weapon and activate box around weapon
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        
+    }
+}
