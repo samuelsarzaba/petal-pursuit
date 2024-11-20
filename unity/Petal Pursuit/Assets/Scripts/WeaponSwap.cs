208f1b93fc38741d429b14bf355495176fd88ab9
samuelsarzaba <samuelsarzaba@gmail.com>
2024-11-20 18:56:40 -0500

    Added enemy follow + damage and weapon swapping (purely visual)

--- unity/Petal Pursuit/Assets/Scripts/WeaponSwap.cs
+++ unity/Petal Pursuit/Assets/Scripts/WeaponSwap.cs
@@ -2,21 +2,102 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponSwap : PlayerMovement
+public class WeaponSwap : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Weapon Settings")]
+    [SerializeField] private GameObject weaponIndicator; // Box indicator for current weapon
+    [SerializeField] private SpriteRenderer weaponSpriteRenderer;
+    [SerializeField] private Sprite shovelSprite;
+    [SerializeField] private Sprite swordSprite;
+    
+    private bool isShovelActive = false;
+    private bool isSwordActive = false;
+    private float weaponDamage = 20f;
+
     void Start()
     {
-        // on click, swap array to player holding other weapon and activate box around weapon
+        // Initialize weapon sprite
+        if (weaponSpriteRenderer == null)
+            weaponSpriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+            
+        UpdateWeaponVisuals();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Check for weapon activation input
+        if (Input.GetMouseButtonDown(1)) // Right click to toggle weapons
+        {
+            ToggleWeapon();
+        }
     }
 
-    void FixedUpdate() {
-        
+    private void ToggleWeapon()
+    {
+        if (!isShovelActive && !isSwordActive)
+        {
+            isShovelActive = true;
+        }
+        else if (isShovelActive)
+        {
+            isShovelActive = false;
+            isSwordActive = true;
+        }
+        else if (isSwordActive)
+        {
+            isSwordActive = false;
+        }
+        UpdateWeaponVisuals();
+    }
+
+    private void UpdateWeaponVisuals()
+    {
+        if (weaponSpriteRenderer != null)
+        {
+            if (isShovelActive && shovelSprite != null)
+            {
+                weaponSpriteRenderer.sprite = shovelSprite;
+            }
+            else if (isSwordActive && swordSprite != null)
+            {
+                weaponSpriteRenderer.sprite = swordSprite;
+            }
+            else
+            {
+                weaponSpriteRenderer.sprite = null;
+            }
+        }
+
+        if (weaponIndicator != null)
+        {
+            weaponIndicator.SetActive(isShovelActive || isSwordActive);
+        }
+    }
+
+    public float GetWeaponDamage()
+    {
+        return (isShovelActive || isSwordActive) ? weaponDamage : 0f;
+    }
+
+    public bool IsShovelActive()
+    {
+        return isShovelActive;
+    }
+
+    public bool IsSwordActive()
+    {
+        return isSwordActive;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy") && Input.GetMouseButton(0) && (isShovelActive || isSwordActive)) // Left click to attack with weapon
+        {
+            Enemy enemy = other.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage((int)GetWeaponDamage());
+            }
+        }
     }
 }
